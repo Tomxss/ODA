0d7de876ad69b30ba0cec7d004daccbaf119b7fa
Theunis Coetzee <tcoetzee@c3r6s3.wethinkcode.co.za>
2020-01-31 23:12:39 +0200

    MongoDb setup

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const handle = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Load models
+const Message = require('./models/message');
+
+// Load keys file
+const Keys = require('./config/keys')
+
+// Debugging
 const debug = require('debug')('app');
 const chalk = require('chalk');
 const port = process.env.PORT || 3000;
 
 const app = express();
+// body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Connect to mlab MongoDb
+mongoose.connect(Keys.MongoDB, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+    debug(chalk.rgb(255, 213, 5)('Connected to MongoDB.'));
+}).catch((err) => {
+    debug(err);
+})
+
 // setup view engine
 app.engine('handlebars', handle({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
@@ -27,6 +48,10 @@ app.get('/contact', (req, res) => {
     });
 });
 
+app.post('/contactUs', (req, res) => {
+    debug(req.body);
+});
+
 app.listen(port, () => {
-    debug(`Server is running optimally on port: ${chalk.magenta(port)}`);
+    debug(chalk.rgb(255, 213, 5)(`Server is running optimally on port: ${chalk.magenta(port)}`));
 });
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://admin:superpassword@matcha-8hkgs.mongodb.net/test?retryWrites=true&w=majority'
+};
